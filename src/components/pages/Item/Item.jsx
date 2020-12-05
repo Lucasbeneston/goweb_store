d3d2c911bd11a5b37fad5f56dbe5eacd6def6c10
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2020-12-05 23:22:23 +0100

    :lipstick: Caroussel design mobile 100% :lipstick:

--- src/components/pages/Item/Item.jsx
+++ src/components/pages/Item/Item.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Items from "../../../data/items";
+
+import "./Item.scss";
 
 export default function Item() {
-  return <main role="main">Je suis la page Item !</main>;
+  // Default image is the first of the array
+  // eslint-disable-next-line no-unused-vars
+  const [selectedImage, setSelectedImage] = useState(Items.images[0]);
+
+  return (
+    <main className="item" role="main">
+      <div className="item_illustration">
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/${selectedImage}`}
+          alt="Illustration du manteau"
+        />
+      </div>
+      <div className="item_miniatures">
+        {Items.images
+          .map((image) => (
+            <div className="item_miniatures_img">
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/${image}`}
+                alt="Illustration du manteau"
+              />
+            </div>
+          ))
+          .slice(0, 4)}
+      </div>
+    </main>
+  );
 }
---
+++ src/data/items.js
@@ -0,0 +1,18 @@
+const items = {
+  title: "Super manteau pour supporter le froid des Hauts-de-France",
+  price: 499.0,
+  images: ["img-1.jpg", "img-2.jpg", "img-3.jpg", "img-4.jpg"],
+  disponibility: "En stock",
+  reference: "#4657",
+  tags: ["Fashion victim", "Classe", "Business"],
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, optio incidunt. Officia quod temporibus possimus molestias, cum, aperiam voluptates laboriosam dolorem minima, fugit architecto praesentium fugiat? Ex in magni tempora.",
+  style: "Regular fit",
+  delivery: "Livraison gratuite",
+  composition: "100% coton",
+  disponibilityColors: "Disponible en 4 couleurs",
+  colors: ["Camel", "Noir", "Bleu marine", "Bordeau"],
+  sizes: ["S", "M", "L", "XL", "XXL"],
+};
+
+export default items;
