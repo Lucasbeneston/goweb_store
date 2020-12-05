a933b45c3f691ff65507bd18fa5f4bdda8369125
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2020-12-05 21:52:09 +0100

    :lipstick: Nav design mobile 80% :lipstick:

--- src/components/molecules/Nav/Nav.jsx
+++ src/components/molecules/Nav/Nav.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faHeart,
+  faSearch,
+  faShoppingBag,
+} from "@fortawesome/free-solid-svg-icons";
+// import LogoGoweb from "../../atoms/SVG/LogoGoweb";
+
+import "./Nav.scss";
 
 export default function Nav() {
-  return <nav role="navigation">Je suis la nav dans le Header !</nav>;
+  return (
+    <nav className="nav" role="navigation">
+      <div className="nav_left">
+        <button className="nav_left_button" type="button">
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <h1>Goweb.</h1>
+        {/* <div className="nav_logo"><LogoGoweb /></div> */}
+      </div>
+
+      <div className="nav_right">
+        <button className="nav_right_button" type="button">
+          <FontAwesomeIcon icon={faHeart} />
+        </button>
+        <button className="nav_right_button" type="button">
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+
+        <button className="nav_right_button" type="button">
+          <FontAwesomeIcon icon={faShoppingBag} />
+        </button>
+      </div>
+    </nav>
+  );
 }
