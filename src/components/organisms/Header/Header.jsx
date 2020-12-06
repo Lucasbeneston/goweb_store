b62e5ffdcb724ac222018c64286382f48dbaaf5c
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2020-12-06 23:15:59 +0100

    :construction: WIP header fixed 50% :construction:

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 import Banner from "../../molecules/Banner/Banner";
 import Nav from "../../molecules/Nav/Nav";
 
+import "./Header.scss";
+
 export default function Header() {
+  useEffect(() => {
+    const headerHeight = document.querySelector(".header").offsetHeight;
+    const body = document.querySelector("body");
+    body.style.marginTop = `${headerHeight}px`;
+  }, []);
+
   return (
-    <header>
+    <header className="header">
       <Banner />
       <Nav />
     </header>
