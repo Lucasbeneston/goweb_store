1326c4d4c138b9cfcfad86b705f47dce0e3b7f24
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2020-12-09 21:27:46 +0100

    :lipstick: NoMatch mobile 100% :lipstick:

--- src/components/pages/NoMatch/NoMatch.jsx
+++ src/components/pages/NoMatch/NoMatch.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./NoMatch.scss";
 
 export default function NoMatch() {
-  return <div>Je suis la page NoMatch !</div>;
+  return (
+    <div className="noMatch">
+      <h2 className="noMatch_oops">Oops...</h2>
+      <h3 className="noMatch_informations">
+        La page demandé n'est pas disponible actuellement !
+      </h3>
+      <Link to="/#" className="noMatch_button">
+        Retour à la page d'accueil
+      </Link>
+    </div>
+  );
 }
