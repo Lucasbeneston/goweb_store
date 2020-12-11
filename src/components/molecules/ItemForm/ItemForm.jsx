036cb696eec383de8531af0a3b0c328d8a9de766
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2020-12-11 18:54:47 +0100

    :recycle: Create new composant itemFormMobile :recycle:

--- src/components/molecules/ItemForm/ItemForm.jsx
+++ src/components/molecules/ItemForm/ItemForm.jsx
@@ -1,121 +1,12 @@
-import React, { useContext, useState, useEffect } from "react";
-import Items from "../../../data/items";
-import { CartContext } from "../../../contexts/CartContext/CartContext";
-import InputSelect from "../../atoms/Inputs/InputSelect/InputSelect";
-import InputNumber from "../../atoms/Inputs/InputNumber/InputNumber";
+import React from "react";
+import ItemFormMobile from "./ItemFormMobile/ItemFormMobile";
 
 import "./ItemForm.scss";
 
 export default function ItemForm() {
-  const { addItem } = useContext(CartContext);
-
-  const [isOpen, setIsOpen] = useState({ openColor: false, openSize: false });
-  const [isDisabled, setIsDisabled] = useState(true);
-  const [selectedValues, setSelectedValues] = useState({
-    id: null,
-    title: Items.title,
-    price: Items.price,
-    color: null,
-    size: null,
-    quantity: 1,
-    image: Items.images[0],
-    reference: Items.reference,
-  });
-
-  const { color, size, quantity } = selectedValues;
-
-  const disabled = () => {
-    if (color !== null && color !== "" && size !== null && size !== "") {
-      setIsDisabled(false);
-      setSelectedValues({
-        ...selectedValues,
-        id: `${selectedValues.size}${selectedValues.quantity}${selectedValues.color}`
-          .replace(" ", "")
-          .toLocaleLowerCase(),
-      });
-    } else {
-      setIsDisabled(true);
-    }
-  };
-
-  useEffect(() => {
-    disabled();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [color, size, quantity]);
-
   return (
     <div className="itemForm">
-      <InputSelect
-        onClickEvent={() => {
-          setIsOpen({ ...isOpen, openColor: !isOpen.openColor });
-        }}
-        isOpen={isOpen.openColor ? "show" : ""}
-        onClickOption={(e) => {
-          setSelectedValues({
-            ...selectedValues,
-            color: e.target.value,
-          });
-        }}
-        id="selectColor"
-        label="Couleur"
-        selectedOption={color || "Choix couleur"}
-        defaultOption="Choix couleur"
-        array={Items.colors}
-      />
-
-      <InputSelect
-        onClickEvent={() => {
-          setIsOpen({ ...isOpen, openSize: !isOpen.openSize });
-        }}
-        isOpen={isOpen.openSize ? "show" : ""}
-        onClickOption={(e) => {
-          setSelectedValues({
-            ...selectedValues,
-            size: e.target.value,
-          });
-        }}
-        id="selectSize"
-        label="Taille"
-        selectedOption={size || "Choix taille"}
-        defaultOption="Choix taille"
-        array={Items.sizes}
-      />
-
-      <div className="itemForm_bottom">
-        <button
-          onClick={() => {
-            addItem(selectedValues);
-          }}
-          className={`itemForm_bottom_buttonAdd ${
-            isDisabled ? "disabled" : null
-          }`}
-          type="button"
-          disabled={isDisabled}
-        >
-          Ajouter au panier
-        </button>
-        <InputNumber
-          id="selectQuantity"
-          label="Quantité"
-          onClickIncrease={() => {
-            if (quantity < 100) {
-              setSelectedValues({
-                ...selectedValues,
-                quantity: quantity + 1,
-              });
-            }
-          }}
-          onClickDecrease={() => {
-            if (quantity > 1) {
-              setSelectedValues({
-                ...selectedValues,
-                quantity: quantity - 1,
-              });
-            }
-          }}
-          defaultOption={quantity}
-        />
-      </div>
+      <ItemFormMobile />
     </div>
   );
 }
