8d4aa6019c0e45b4d2497646b3a1b6592fcef234
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2020-12-14 14:47:22 +0100

    :sparkles: Opacity  miniature selected :sparkles:

--- src/components/organisms/CarouselImages/CarouselImages.jsx
+++ src/components/organisms/CarouselImages/CarouselImages.jsx
@@ -8,9 +8,10 @@ import Items from "../../../data/items";
 import "./CarouselImages.scss";
 
 export default function CarrouselImages() {
-  const [selectedImage, setSelectedImage] = useState(
-    `assets/${Items.images[0]}`
-  );
+  const [selectedImage, setSelectedImage] = useState({
+    src: `assets/${Items.images[0]}`,
+    index: 0,
+  });
 
   return (
     <>
@@ -22,27 +23,31 @@ export default function CarrouselImages() {
       </HashLink>
       <div className="carouselImages_illustration">
         <img
-          src={`${process.env.PUBLIC_URL}/${selectedImage}`}
+          src={`${process.env.PUBLIC_URL}/${selectedImage.src}`}
           alt="Illustration du vêtement selectionné"
         />
       </div>
       <div className="carouselImages_miniatures">
         {Items.images
-          .map((image) => (
+          .map((image, index) => (
             <div
-              className="carouselImages_miniatures_img"
+              className={`carouselImages_miniatures_img ${
+                index === selectedImage.index ? "selected" : null
+              }`}
               key={image}
               tabIndex={0}
               role="button"
               onKeyDown={(e) => {
-                setSelectedImage(
-                  e.target.src.substring(e.target.baseURI.length)
-                );
+                setSelectedImage({
+                  src: e.target.src.substring(e.target.baseURI.length),
+                  index,
+                });
               }}
               onClick={(e) => {
-                setSelectedImage(
-                  e.target.src.substring(e.target.baseURI.length)
-                );
+                setSelectedImage({
+                  src: e.target.src.substring(e.target.baseURI.length),
+                  index,
+                });
               }}
             >
               <img
